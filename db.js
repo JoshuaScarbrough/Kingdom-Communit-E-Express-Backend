e1e1a196683534debe15e5b5c4404cd0f6bde28f
Joshua Scarbrough <joshscar1418@gmail.com>
2025-05-26 20:41:06 -0500

    Changed the connection string hoping for a connection

--- db.js
+++ db.js
@@ -19,7 +19,7 @@ let DB_URI;
 if (process.env.NODE_ENV === "test"){
     DB_URI = "postgresql://postgres:password@localhost:5432/capstone2_test";
 } else {
-    DB_URI = "postgresql://postgres.mlqzpjcyorxhfiurzirb:password@aws-0-us-east-2.pooler.supabase.com:6543/postgres?sslmode=require"
+    DB_URI = "postgresql://postgres.mlqzpjcyorxhfiurzirb:password@aws-0-us-east-2.pooler.supabase.com:5432/postgres?sslmode=require";
 }
 
 
